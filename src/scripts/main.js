4e7aecf5297af6ecf07c2808541e43de08d23533
Vladimir <dream.isdestiny@mail.ru>
2021-02-17 19:32:31 +0600

    feat: added multiple .pug serve, changed tailwind config, updated tailwind

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 /**
  * An example ES6 module:
  */
-import sayHello from './utils/sayHello';
-window.sayHello = sayHello
+import sayHello from "./utils/sayHello";
+window.sayHello = sayHello;
+window.sayHello("Parcel JS user");
